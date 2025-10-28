044b7292a13a291367720b4a00168efb952fb175
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2025-10-28 12:57:40 +0000

    Convert to Webpack repository

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+
+function getCountries() {
+  fetch("https://restcountries.com/v3.1/all?fields=name")
+    .then((res) => res.json())
+    .then((data) => {
+      const select = document.getElementById("country");
+      const countries = data.sort((a, b) =>
+        a.name.common.localeCompare(b.name.common)
+      );
+      countries.forEach((country) => {
+        const option = document.createElement("option");
+        option.textContent = country.name.common;
+        select.appendChild(option);
+      });
+    });
+}
+function getCountryCode(country) {
+  fetch(`https://restcountries.com/v3.1/name/${country.toLowerCase()}`)
+    .then((res) => res.json())
+    .then(([data]) => {
+      let countryCode = data.cca2;
+      console.log(countryCode);
+    });
+}
+
+function validateForm() {
+  const form = document.querySelector("form");
+  const formInputs = document.querySelectorAll(".form-field");
+  let targetInput;
+  //   const formInput = document.querySelectorAll("form input");
+  form.addEventListener("input", (e) => {
+    targetInput = document.querySelector(`#${e.target.id}`);
+    toggleError(targetInput);
+  });
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formInputs.forEach((input) => {
+      if (input.id !== "form-submit") {
+        toggleError(input);
+      }
+    });
+  });
+  function toggleError(input) {
+    let inputError = document.querySelector(`#${input.id}-error`);
+    if (input.validity.valid) {
+      inputError.textContent = "";
+    } else if (input.validity.valueMissing) {
+      inputError.textContent = `*${
+        input.id.slice(0, 1).toUpperCase() + input.id.slice(1)
+      } field must be filled.`;
+    }
+  }
+}
+getCountries();
+validateForm();
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
